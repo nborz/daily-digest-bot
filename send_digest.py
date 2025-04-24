5de0b527ea017635c437d2ecf3cab1bdea9aad36
nborz <hello@borzunov.com>
2025-04-24 22:58:21 +0200

    Update send_digest.py

--- send_digest.py
+++ send_digest.py
@@ -1,9 +1,13 @@
 import requests
 import os
 
+# Retrieve the token from GitHub Actions secrets
 TOKEN = os.getenv("BOT_TOKEN")
+
+# Replace with your actual chat/channel ID
 CHAT_ID = "-1002693285869"
 
+# Load message content from a file
 with open("digest_message.txt", "r", encoding="utf-8") as f:
     MESSAGE = f.read()
 
@@ -15,6 +19,7 @@ def send_to_telegram():
         "parse_mode": "HTML",
         "disable_web_page_preview": True
     }
+
     response = requests.post(url, data=payload)
     print("Status Code:", response.status_code)
     print("Response Text:", response.text)
